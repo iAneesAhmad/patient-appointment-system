9b2d06e33478a61f0dde38097f4c7afbc9ef236f
Anees Ahmad <106009135+iAneesAhmad@users.noreply.github.com>
2024-05-02 01:01:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function showContainer(id) {
+    var id = document.getElementById(id);
+    id.style.display= "block";
+    
+}
+function hideContainer(id) {
+    var id = document.getElementById(id);
+    id.style.display= "none";
+}
+function patientInfoForm() {
+    
+}
